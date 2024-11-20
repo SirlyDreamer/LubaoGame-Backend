45228d5e393f224d1d80ff233e726a92ef33a58c
SirlyDreamer <45280500+SirlyDreamer@users.noreply.github.com>
2024-11-20 10:49:08 +0800

    Allow CORS

--- server.py
+++ server.py
@@ -2,8 +2,17 @@ import os
 from pydoc import describe
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from utils import LevelDatabase
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 level_db = LevelDatabase(
     host=os.getenv("POSTGRES_HOST", "localhost"),
